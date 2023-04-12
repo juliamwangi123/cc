dadf8da8f970937d37dddc279d62d83bb714bd0e
Julie <juliamwangi123>
2023-04-12 13:35:41 +0300

    get user

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -29,7 +29,7 @@ const signUpUser = async (req, res) =>{
         const user = await User.login(email, password)
         const token =generateToken(user._id)
 
-        res.status(200).json({email, token})
+        res.status(200).json({user, token})
 
     }catch(error){
         res.status(400).json({error:error.message})
