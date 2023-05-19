226a574070ac8c9647f4e9cd3926348f62f2ec6b
Julie <juliamwangi123>
2023-05-19 12:18:36 +0300

    use verfy  method to verify the token

--- Middleware/verifyToken.js
+++ Middleware/verifyToken.js
@@ -1,6 +1,20 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
 
 const requireAuth = (req, res, next) =>{
     //get token from headers
     const token = req.headers.authorization;
+
+    //verify token if it exists
+    if(token){
+        jwt.verify(token, process.env.SECRET, (err, decodedTokem)=>{
+            if (err) {
+                console.log({ err: err.message })
+                return res.status(403).json({ err: err.message });
+              }
+          
+        })
+
+    }
 }
